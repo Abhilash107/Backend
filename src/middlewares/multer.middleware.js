1f59575a0bcc3e1286289d92b0f947cd359e24b9
Abhilash107 <abhilash00003108884@gmail.com>
2024-03-04 21:14:07 +0530

    done

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null,'/TEMP')
+      cb(null,"/")
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)    
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
   
 export const upload = multer(
     { 
-      storage: storage 
+      storage: storage,
     });
 
 
